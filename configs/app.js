942d1ff36ca37759fe6cb9a9dd5edc24833cdb23
dmarroquin2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-02-14 11:24:22 -0600

    Se agrega configs

---
+++ configs/app.js
@@ -0,0 +1,35 @@
+import express from "express"
+import cors from 'cors'
+import helmet from "helmet"
+import morgan from "morgan"
+
+import productRoutes from '../src/product/product.routes.js'
+import categoryRoutes from '../src/category/category.routes.js'
+
+const configs = (app)=>{
+    app.use(express.json())
+    app.use(express.urlencoded({extended:false}))
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan('dev'))
+}
+
+const routes = (app)=>{
+    app.use('/v1/product', productRoutes)
+    app.use('/v1/category', categoryRoutes)
+}
+
+
+
+export const initServer = ()=>{
+    const app = express()
+    try {
+        configs(app)
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running in port ${process.env.PORT}`);
+    } catch (error) {
+        console.error(`Server init failed`,error);
+        
+    }
+}
