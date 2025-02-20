d5ded21b449c803db03368c276b7b245f4762e00
dmarroquin2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-02-20 10:01:04 -0600

    Se arregla problemas de app.js

--- configs/app.js
+++ configs/app.js
@@ -3,15 +3,14 @@ import cors from 'cors'
 import helmet from "helmet"
 import morgan from "morgan"
 
-<<<<<<< HEAD
 import userRouter from '../src/user/user.routes.js'
 import productRoutes from '../src/product/product.routes.js'
 import categoryRoutes from '../src/category/category.routes.js'
 import authRoutes from '../src/auth/auth.routes.js'
-=======
+
 import productRoutes from '../src/product/product.routes.js'
 import categoryRoutes from '../src/category/category.routes.js'
->>>>>>> 942d1ff36ca37759fe6cb9a9dd5edc24833cdb23
+
 
 const configs = (app)=>{
     app.use(express.json())
