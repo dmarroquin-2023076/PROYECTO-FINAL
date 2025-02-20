1b6a4ac1059b479e1b75e4b2b037650f5a06e68c
dmarroquin2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-02-20 10:00:18 -0600

    Se corrije errores de app.js

--- configs/app.js
+++ configs/app.js
@@ -24,11 +24,9 @@ const configs = (app)=>{
 const routes = (app)=>{
     app.use('/v1/product', productRoutes)
     app.use('/v1/category', categoryRoutes)
-<<<<<<< HEAD
     app.use('/v1/user', userRouter)
     app.use('/v1',authRoutes)
-=======
->>>>>>> 942d1ff36ca37759fe6cb9a9dd5edc24833cdb23
+
 }
 
 
